2a61e5b346d678771cc10ed6b57d82c9241afce4
EC2 Default User <ec2-user@ip-172-31-85-79.ec2.internal>
2021-09-09 15:02:15 +0000

    [update]ルートパス設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,9 +37,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book=Book.find(params[:id])
-    book.destroy
+    @book=Book.find(params[:id])
+    if @book.destroy
+      flash[:destroyed]="Book was successfully destroyed."
     redirect_to books_path
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   # get 'books/new'
   # get 'books/show'
   # get 'books/edit'
-  get 'top' => "homes#top"
+  # get 'top' => "homes#top"
+  
+  # ルートパスの設定
+  root to: "homes#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# ルートをまとめて
  resources :books
+ 
 end
